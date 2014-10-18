207ed4b55d8a50bda9d994e81e33c5739e507137
Michael Richardson <DragonRider0o0@gmail.com>
2014-10-18 12:58:59 -0400

    Began Venmo Integration

---
+++ Development/RunForYourMoney/Controllers/VenmoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RunForYourMoney.Controllers
+{
+    public class VenmoController : Controller
+    {
+        public ActionResult Index(string access_token)
+        {
+            ViewBag.access_token = access_token;
+
+            return View();
+        }
+    }
+}
---
+++ Development/RunForYourMoney/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RunForYourMoney.Startup))]
+namespace RunForYourMoney
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
